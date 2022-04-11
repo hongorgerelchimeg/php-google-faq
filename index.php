094d8e87551afb973655cd1f05245e5d4df57fde
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-04-11 16:45:44 +0200

    complete

--- index.php
+++ index.php
@@ -37,3 +37,23 @@ $arr_faq = [
     ]
 ];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google FAQ</title>
+</head>
+<body>
+    <?php
+        foreach ($arr_faq as $article) {
+            echo '<div class="article">';
+            echo $article['title'];
+            echo $article['html'];
+            echo "</div>";
+        }
+    ?>
+</body>
+</html>
